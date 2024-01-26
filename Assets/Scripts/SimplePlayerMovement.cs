4d6fbab676a210db7756fbbfd3389c9c99dbf9a0
Ciaran Williams <ciaran2.williams@uwe.live.ac.uk>
2024-01-26 21:59:09 +0000

    simple movement for a sprite

---
+++ Assets/Scripts/SimplePlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rigidbody;
+    private Vector2 direction;
+
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        direction = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rigidbody.velocity = new Vector2 (direction.x * speed, direction.y * speed);
+    }
+
+
+    
+}
