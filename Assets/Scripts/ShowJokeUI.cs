b6868b27be32dde20b98fc6e07ad9e58bc0d6e92
Yat Long Chan <yat3.chan@live.uwe.ac.uk>
2024-01-28 01:37:38 +0800

    Joke counter

--- Assets/Scripts/ShowJokeUI.cs
+++ Assets/Scripts/ShowJokeUI.cs
@@ -5,21 +5,30 @@ using TMPro;
 
 public class ShowJokeUI : MonoBehaviour
 {
-    public GameObject child_text;
+    public GameObject joke_text;
+    public GameObject counter_text;
     private TextMeshProUGUI text_comp;
 
     void Start()
     {
-        text_comp = child_text.GetComponent<TextMeshProUGUI>();
+        SetCounter(0);
     }
 
     public void SetJoke(string s)
-    {   
+    {
+        text_comp = joke_text.GetComponent<TextMeshProUGUI>();
         text_comp.text = s;
     }
 
     public void SetVisible(bool flag)
     {
+        text_comp = joke_text.GetComponent<TextMeshProUGUI>();
         text_comp.enabled = flag;
+    }
+
+    public void SetCounter(int n)
+    {
+        text_comp = counter_text.GetComponent<TextMeshProUGUI>();
+        text_comp.text = "Jokes: " + n;
     }
 }
