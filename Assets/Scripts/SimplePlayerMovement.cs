c428ed842cebe998941908404f88a5bbb69c2c94
Yat Long Chan <yat3.chan@live.uwe.ac.uk>
2024-01-27 21:05:52 +0800

    Player freeze when collided with enemy

--- Assets/Scripts/SimplePlayerMovement.cs
+++ Assets/Scripts/SimplePlayerMovement.cs
@@ -8,6 +8,13 @@ public class SimplePlayerMovement : MonoBehaviour
     public Rigidbody2D rigidbody;
     private Vector2 direction;
 
+    public float freeze_clock;
+
+    void Start()
+    {
+        freeze_clock = 0f;
+    }
+
     void Update()
     {
         ProcessInputs();
@@ -15,7 +22,16 @@ public class SimplePlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Move();
+        if (freeze_clock > 0f)
+        {
+            freeze_clock -= Time.deltaTime;
+            rigidbody.velocity = new Vector2(0f, 0f);
+        }
+
+        else
+        {
+            Move();
+        }
     }
 
 
@@ -32,6 +48,8 @@ public class SimplePlayerMovement : MonoBehaviour
         rigidbody.velocity = new Vector2 (direction.x * speed, direction.y * speed);
     }
 
-
-    
+    public void FreezePlayer()
+    {
+        freeze_clock = 10f;
+    }
 }
