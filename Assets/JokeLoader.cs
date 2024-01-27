e0eba9bff217a2616a9fd2c5630f268268189895
Ciaran Williams <ciaran2.williams@live.uwe.ac.uk>
2024-01-27 15:49:04 +0000

    fixed jokes being added

--- Assets/JokeLoader.cs
+++ Assets/JokeLoader.cs
@@ -2,22 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Linq;
+
 public class JokeLoader : MonoBehaviour
 {
-    public TMP_Text text;
+    //public TMP_Text text;
+    public string test;
+    private List<string> jokeslist;
 
-    private void Awake()
+/*    private void Awake()
     {
         text.SetText("Lol");
-    }
+    }*/
     void Start()
     {
-        
+        jokeslist = new List<string> {"your mom", "Bob", "Cat" };
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    public void jokelist(int id)
+    {
+        test = jokeslist[id];
         
     }
 }
--- Assets/Scripts/PlayerJokes.cs
+++ Assets/Scripts/PlayerJokes.cs
@@ -5,19 +5,33 @@ using UnityEngine;
 public class PlayerJokes : MonoBehaviour
 {
     public Joke[] jokes;
+    public JokeLoader loader;
     public const int jokes_limit = 3;
     public int jokes_count = 0;
+    public int randnum;
 
     private void Start()
     {
         jokes = new Joke[jokes_limit];
+        randomiser();
+        
     }
 
     void Update()
-    {
+    {   
+        loader.jokelist(randnum);
         jokes[jokes_count] = ScriptableObject.CreateInstance<Joke>();
-        jokes[jokes_count].setJoke("Test");
+        jokes[jokes_count].setJoke(loader.test);
+        print(jokes[jokes_count].content);
         jokes_count++;
+        randomiser();
         gameObject.SetActive(false);
+        
+    }
+
+    private void randomiser()
+    {
+        randnum = Random.Range(0, 2);
+        print(randnum);
     }
 }
