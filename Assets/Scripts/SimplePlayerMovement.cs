a8b8b28956b2c600379105f8883a6f36cacba981
MonikaBuk# <monika2.bukovics@live.uwe.ac.uk>
2024-01-27 16:02:30 +0000

    Player animation working

--- Assets/Scripts/SimplePlayerMovement.cs
+++ Assets/Scripts/SimplePlayerMovement.cs
@@ -11,9 +11,12 @@ public class SimplePlayerMovement : MonoBehaviour
 
     public float freeze_clock;
 
+    private Animator animator;
+
     void Start()
     {
         freeze_clock = 0f;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -47,10 +50,22 @@ public class SimplePlayerMovement : MonoBehaviour
     void Move()
     {
         rigidbody.velocity = new Vector2 (direction.x * speed, direction.y * speed);
+        animator.SetFloat("Horizontal", direction.x);
+        animator.SetFloat("Vertical", direction.y);
+        if (direction.x != 0f)
+        {
+            animator.SetBool("GoingToSide", true);
+        }
+        else
+        {
+            animator.SetBool("GoingToSide", false);
+        }
     }
 
     public void FreezePlayer()
     {
         freeze_clock = 10f;
     }
+
+
 }
